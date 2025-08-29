dde5c70bc6745dad130eac1888287a9f72ece956
n <2585803+tt5@users.noreply.github.com>
2025-08-29 23:42:52 +0200

    Add Navbar component and update routing

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,18 @@
 import "./app.css";
-import SimpleComponent from "./components/SimpleComponent";
+import { Suspense } from "solid-js";
+import { Router } from "@solidjs/router";
+import { FileRoutes } from "@solidjs/start/router";
+import Navbar from "./components/Navbar";
 
 export default function App() {
   return (
-    <main>
-      <SimpleComponent />
-    </main>
+    <Router root={(props) => (
+      <>
+        <Navbar />
+        <Suspense>{props.children}</Suspense>
+      </>
+    )}>
+      <FileRoutes />
+    </Router>
   );
 }
---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,20 @@
+import { A } from "@solidjs/router";
+
+export default function Navbar() {
+  return (
+    <nav>
+      <ul style={{
+        display: 'flex',
+        'list-style': 'none',
+        padding: '1rem',
+        gap: '1rem',
+        'background-color': '#f0f0f0',
+        margin: 0
+      }}>
+        <li><A href="/">Home</A></li>
+        <li><A href="/about">About</A></li>
+        <li><A href="/links">Links</A></li>
+      </ul>
+    </nav>
+  );
+}
