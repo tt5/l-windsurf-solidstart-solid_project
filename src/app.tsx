e2a5c3a07dc2ab4ee1726372062836d7b7aad275
n <2585803+tt5@users.noreply.github.com>
2025-08-29 22:50:33 +0200

    Add SimpleComponent and update App component

---
+++ src/app.tsx
@@ -0,0 +1,10 @@
+import "./app.css";
+import SimpleComponent from "./components/SimpleComponent";
+
+export default function App() {
+  return (
+    <main>
+      <SimpleComponent />
+    </main>
+  );
+}
